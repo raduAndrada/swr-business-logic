10aa9107de0ea3de991b11941bc948ccfb6c4dff
andrada radu <irina-andrada.radu@ing.com>
2024-05-27 09:57:48 +0300

    fix: step corrected

---
+++ swr-menu/src/main/java/ro/swr/menu/SwrMenuApplication.java
@@ -0,0 +1,12 @@
+package ro.swr.menu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwrMenuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwrMenuApplication.class, args);
+    }
+}
