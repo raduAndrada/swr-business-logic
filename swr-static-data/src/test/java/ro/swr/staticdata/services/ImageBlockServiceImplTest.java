eb06ebc107bdef93c8354ad696c6c967fd7e3c1d
andrada radu <irina-andrada.radu@ing.com>
2024-10-27 08:25:50 +0200

    fix: disabled test

--- swr-static-data/src/test/java/ro/swr/staticdata/services/ImageBlockServiceImplTest.java
+++ swr-static-data/src/test/java/ro/swr/staticdata/services/ImageBlockServiceImplTest.java
@@ -1,5 +1,6 @@
 package ro.swr.staticdata.services;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -13,6 +14,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@Disabled
 class ImageBlockServiceImplTest {
 
     @Autowired
